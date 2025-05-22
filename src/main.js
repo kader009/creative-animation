02eda197bbfeb05886f208c7138b0f57c2da7a0b
kader009 <kadermollarasel000@gmail.com>
2025-05-22 10:40:20 +0600

    box animation on scroll

--- src/main.js
+++ src/main.js
@@ -2,19 +2,12 @@ import './style.css';
 
 const box = document.getElementById('box');
 
-gsap.from(box, {
-  x: -200,
-  opacity: 0,
-  duration: 1,
-  ease: 'circ',
-  delay: 1,
-});
-
-gsap.to('.loader', {
-  opacity:0,
-  duration:1,
-  delay:2,
-  onComplete: () =>{
-    document.querySelector('.loader').style.display = 'none'
+gsap.to(box, {
+  x: 400,
+  yoyo:true,
+  scrollTrigger:{
+    trigger: box,
+    scrub:true,
+    start:'top center'
   }
-})
+});

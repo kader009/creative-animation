f5c11aa61f9ff88b08fdc558e31ce8e05dcbb217
kader009 <kadermollarasel000@gmail.com>
2025-05-22 21:57:17 +0600

    right to left scroll structure done

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
 import './style.css';
 
-const box = document.getElementById('box');
 
-gsap.to(box, {
-  x: 400,
-  yoyo:true,
-  scrollTrigger:{
-    trigger: box,
-    scrub:true,
-    start:'top center'
-  }
-});

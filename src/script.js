df499282252b8b39d0dd560dc7e19fb22cf333b6
kader009 <kadermollarasel000@gmail.com>
2025-05-31 22:51:34 +0600

    practice about clousure

--- src/script.js
+++ src/script.js
@@ -1,13 +1,10 @@
-const original = {
-  name:'kader',
-  address:{
-    city: 'Dhaka'
-  }
+function couter() {
+  let count = 100;
+
+  return function Increase(add) {
+    console.log(`the amount is ${count + add}`);
+  };
 }
 
-const shallowCopy = {...original}
-shallowCopy.name = 'Jane'
-shallowCopy.address.city = 'Noakhali'
-
-console.log(original.name);
-console.log(original.address.city);
+const addAllvalue = couter();
+addAllvalue(300);
